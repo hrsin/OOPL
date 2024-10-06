a35bbb485bf0e48b02a7f939e1e498cb7208d819
hr <80281605+hrsin@users.noreply.github.com>
2024-10-06 14:47:04 +0530

    Throwing and Catching Arithmetic exception

--- Module 3/1-20/Q1.java
+++ Module 3/1-20/Q1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 class Q1 {
-  public static void main(String args[]) {
-    System.out.println("Q1 Mod 3 java");
-  }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numerator: ");
+        float a = sc.nextFloat();
+        System.out.print("Enter denominator: ");
+        float b = sc.nextFloat();
+        float result = a/b;
+        try {
+            if (b>0) {
+                System.out.println("A/B is: "+ result);
+            } else {
+                throw new ArithmeticException("Denominator cannot be 0");
+            }
+        } catch(ArithmeticException e) {
+            System.out.println("Exception Handled: " + e);
+        }
+    }
 }
